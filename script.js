4521ce021f1f704d11e55587a6681144ae925fc9
Scarygami <scarygami@gmail.com>
2017-05-04 01:02:03 +0200

    Update to newest Google Takeout data format

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2012 Gerwin Sturm, FoldedSoft e.U. / www.foldedsoft.at
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may
  * not use this file except in compliance with the License. You may obtain
  * a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  */
 
 (function (global) {
-  "use strict";
+  'use strict';
 
   var $ = global.$, google = global.google;
 
@@ -30,7 +30,7 @@
       h = this.getHours().toString();
       min = this.getMinutes().toString();
       sec = this.getSeconds().toString();
-      return y + "-" + (m[1] ? m : "0" + m[0]) + "-" + (d[1] ? d : "0" + d[0]) + " " + (h[1] ? h : "0" + h[0]) + ":" + (min[1] ? min : "0" + min[0]) + ":" + (sec[1] ? sec : "0" + sec[0]);
+      return y + '-' + (m[1] ? m : '0' + m[0]) + '-' + (d[1] ? d : '0' + d[0]) + ' ' + (h[1] ? h : '0' + h[0]) + ':' + (min[1] ? min : '0' + min[0]) + ':' + (sec[1] ? sec : '0' + sec[0]);
     };
 
     function updateLine() {
@@ -38,14 +38,14 @@
       if (path.getLength() > maxMarkers) {
         path.removeAt(0);
       }
-      latlng = new google.maps.LatLng(locations[pos].latitude, locations[pos].longitude);
-      $("#time").html((new Date(locations[pos].timestampMs)).niceDate());
+      latlng = new google.maps.LatLng(locations[pos].latitudeE7 / 10000000, locations[pos].longitudeE7 / 10000000);
+      $('#time').html((new Date(locations[pos].timestampMs)).niceDate());
       path.push(latlng);
       if (map.getBounds() && !map.getBounds().contains(latlng)) {
         map.setCenter(latlng);
       }
       lineHeader.setPosition(latlng);
-      $("#pos").val((numLocations - pos) / numLocations * 100);
+      $('#pos').val((numLocations - pos) / numLocations * 100);
       pos -= 1;
       if (pos >= 0) {
         global.setTimeout(updateLine, (100 - speed) * 3);
@@ -55,7 +55,7 @@
     }
 
     map = new google.maps.Map(
-      document.getElementById("map_canvas"),
+      document.getElementById('map_canvas'),
       {
         zoom: 9,
         center: new google.maps.LatLng(0, 0),
@@ -64,7 +64,7 @@
     );
 
     polyLine = new google.maps.Polyline({
-      strokeColor: "black",
+      strokeColor: 'black',
       strokeOpacity: 1.0,
       strokeWeight: 2,
       map: map
@@ -74,29 +74,29 @@
       icon: {
         path: google.maps.SymbolPath.CIRCLE,
         scale: 5,
-        fillColor: "yellow",
+        fillColor: 'yellow',
         fillOpacity: 1,
-        strokeColor: "black",
+        strokeColor: 'black',
         strokeWeight: 1
       },
       map: map
     });
 
-    if (global.latitudeJsonData && global.latitudeJsonData.data && global.latitudeJsonData.data.items && global.latitudeJsonData.data.items.length > 0) {
-      locations = global.latitudeJsonData.data.items;
+    if (global.locationJsonData && global.locationJsonData.locations && global.locationJsonData.locations.length > 0) {
+      locations = global.locationJsonData.locations;
       numLocations = locations.length;
       pos = numLocations - 1;
       running = true;
       updateLine();
     } else {
-      console.log("No JSON Data available, make sure input.js is loaded and sets the window.latitudeJsonData variable");
+      console.log('No JSON Data available, make sure input.js is loaded and sets the window.locationJsonData variable');
     }
 
-    $("#speed").change(function () {
-      speed = parseInt($("#speed").val(), 10);
+    $('#speed').change(function () {
+      speed = parseInt($('#speed').val(), 10);
     });
-    $("#pos").change(function () {
-      pos = numLocations - Math.floor(numLocations * parseInt($("#pos").val(), 10) / 100);
+    $('#pos').change(function () {
+      pos = numLocations - Math.floor(numLocations * parseInt($('#pos').val(), 10) / 100);
       pos = Math.min(Math.max(0, pos), numLocations - 1);
       if (!running) {
         running = true;
